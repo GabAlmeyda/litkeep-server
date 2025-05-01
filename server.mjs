85d88e353478e2c2a32cfdd34c955f0a6bc42b86
Gabriel Almeida <gabrielalmeida7258@gmail.com>
2025-05-01 16:46:02 -0300

    Correção de erros

--- server.mjs
+++ server.mjs
@@ -3,10 +3,10 @@ import jsonServer from "json-server";
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port, "0.0.0.0", () =>
-    console.log(`JSON server is running on port ${port}`)
-);
+server.listen(port, "0.0.0.0", () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
