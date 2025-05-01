0c9b51596fdde65cfccbbde875bb15ab9bd0847e
Gabriel Almeida <gabrielalmeida7258@gmail.com>
2025-05-01 14:43:02 -0300

    ESM

--- server.mjs
+++ server.mjs
@@ -1,8 +1,9 @@
-const jsonServer = require("json-server");
+import jsonServer from "json-server";
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 server.use(middlewares);
 server.use(router);
